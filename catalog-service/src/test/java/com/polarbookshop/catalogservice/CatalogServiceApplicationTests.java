9e6f1e34e02431e594a2aa38005a99a0a836f48f
Gaurav Kamble <gauravkamble909@gmail.com>
2024-11-19 21:40:51 +0530

    integration tests @SpringBootTest

--- catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,10 +1,33 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	public void whenPostRequestThenBookCreated(){
+		var expectedBook = new Book("1213121414", "Title", "Author", 100.0);
+
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(actualBook -> {
+					Assertions.assertThat(actualBook).isNotNull();
+					Assertions.assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+				});
+	}
 
 	@Test
 	void contextLoads() {
